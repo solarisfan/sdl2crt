0146dd3e89c6dc1e354ef41563da707a64e8aa84
solarisfan <84895244+solarisfan@users.noreply.github.com>
2026-05-30 15:56:05 +0800

    Remove useless code

--- src/vgafont.pas
+++ src/vgafont.pas
@@ -3,7 +3,6 @@ unit vgafont;
 {$DEFINE VGAFONT}
 {$I renderChar.inc}
 
-
 initialization
 begin
 	writeln(stderr, 'Initializing vgafont unit');
@@ -16,12 +15,6 @@ begin
 	fontAry[M80x50].fileName := 'vga9x8.ttf';
 	fontAry[M80x50].data := @VGA9x8[0];
 	fontAry[M80x50].size := Length(VGA9x8);
-//	fontFile := 'cga.ttf';
-//	fontFile := 'cga-2y.ttf';
-//	fontFile := 'ega8x14.ttf';
-//	fontFile := '/usr/share/fonts/truetype/ubuntu/UbuntuMono[wght].ttf';
-//	fontFile := '/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf';
-//	tty_init;
 end;
 
 finalization
