4b34432361f6909c282684fb0133a84f36275af0
solarisfan <84895244+solarisfan@users.noreply.github.com>
2026-08-20 12:43:07 +0800

    Initialize font data to nil

--- src/nofont.pas
+++ src/nofont.pas
@@ -1,16 +1,28 @@
 unit nofont;
 
-{$DEFINE VGAFONT}
+{$DEFINE NOFONT}
 {$I renderChar.inc}
 
 
 initialization
 begin
+	fontAry[S40x25].fileName := '';
+	fontAry[S40x25].ptsize := 0;
+	fontAry[S40x25].data := nil;
+	fontAry[S40x25].size := 0;
+	fontAry[S80x25].fileName := '';
+	fontAry[S40x25].ptsize := 0;
+	fontAry[S80x25].data := nil;
+	fontAry[S80x25].size := 0;
+	fontAry[S80x50].fileName := '';
+	fontAry[S80x50].ptsize := 0;
+	fontAry[S80x50].data := nil;
+	fontAry[S80x50].size := 0;
 end;
 
 finalization
 begin
-	writeln(stderr, 'Freetype finalize');
+	Logger.log('Freetype finalize');
 end;
 
 end.
