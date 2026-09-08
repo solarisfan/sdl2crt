1d9251216d6dc302ef2f1430972f4f423dfa09bb
solarisfan <84895244+solarisfan@users.noreply.github.com>
2026-09-08 12:41:50 +0800

    Bug fixes
    Bug fixes

--- src/vgafont.pas
+++ src/vgafont.pas
@@ -18,10 +18,12 @@ begin
 	fontAry[S40x25].ptsize := 16;
 	fontAry[S40x25].data := @VGA9x16x2[0];
 	fontAry[S40x25].size := Length(VGA9x16x2);
+	
 	fontAry[S80x25].fileName := DefaultFontFile80X25;
-	fontAry[S40x25].ptsize := 12;
+	fontAry[S80x25].ptsize := 12;
 	fontAry[S80x25].data := @VGA9x16[0];
 	fontAry[S80x25].size := Length(VGA9x16);
+	
 	fontAry[S80x50].fileName := DefaultFontFile80X50;
 	fontAry[S80x50].ptsize := 10;
 	fontAry[S80x50].data := @VGA9x8[0];
